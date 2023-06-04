bfa3a1dadd1faa7cb70136a151d5f76c4f1d4341
Gill-Singh-A <amansg22@iitk.ac.in>
2023-06-05 01:17:12 +0530

    Added ROS Exercise 1 Solution

---
+++ Assignments/ROS/Exercise_1/run.sh
@@ -0,0 +1,6 @@
+cd ~/Workspace/smb_common/
+source devel/setup.bash
+cd ~/Workspaces/teleop_twist_keyboard/
+source devel/setup.bash
+roslaunch smb_gazebo exercise_1.launch
+cd ~
---
+++ Assignments/ROS/Exercise_1/setup.bash
@@ -0,0 +1,20 @@
+sudo apt install ros-noetic-hector-gazebo-plugins ros-noetic-velodyne ros-noetic-velodyne-description ros-noetic-velodyne-gazebo-plugins ros-noetic-pointcloud-to-laserscan ros-noetic-twist-mux
+cd ~
+mkdir Git Workspace
+cd Workspace
+mkdir -p smb_ws/src/
+mkdir -p teleop_twist_keyboard/src/
+cd ~/Git/
+wget https://ethz.ch/content/dam/ethz/special-interest/mavt/robotics-n-intelligent-systems/rsl-dam/ROS2021/lec1/smb_common.zip
+git clone https://github.com/ros-teleop/teleop_twist_keyboard.git
+unzip smb_common.zip
+rm smb_common.zip
+ln -s ~/Git/smb_common/* ~/Workspaces/smb_ws/src/
+ln -s ~/Git/teleop_twist_keyboard/ ~/Workspaces/teleop_twist_keyboard/src/
+cd ~/Workspace/smb_common/
+catkin build
+source devel/setup.bash
+cd ~/Workspaces/teleop_twist_keyboard/
+catkin build
+source devel/setup.bash
+cd ~
